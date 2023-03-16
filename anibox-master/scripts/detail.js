074ff3e48145ed17ecbd96dbd7e9236290516f36
moh yudi aftoni <mohyudiaftoni@gmail.com>
2023-03-17 00:33:28 +0700

    menambah detail.js

--- anibox-master/scripts/detail.js
+++ anibox-master/scripts/detail.js
@@ -1,39 +1,61 @@
-import { detail as data } from './data.js'; //? hapus setelah data sudah didapat dari API
-import { initialSetup } from './index.js';
+import { detail as data } from "./data.js"; //? hapus setelah data sudah didapat dari API
+import { initialSetup } from "./index.js";
 
 initialSetup();
 
-const titleEl = document.getElementById('title');
-const descriptionEl = document.getElementById('description');
-const bannerImageEl = document.getElementById('banner-image');
-const coverImageEl = document.getElementById('cover-image');
-const infoEl = document.getElementById('info');
+const titleEl = document.getElementById("title");
+const descriptionEl = document.getElementById("description");
+const bannerImageEl = document.getElementById("banner-image");
+const coverImageEl = document.getElementById("cover-image");
+const infoEl = document.getElementById("info");
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
-const id = urlParams.get('id');
+const id = urlParams.get("id");
 
 //? fetch data dari API
 // const data = ....
 
 //? masukkan data ke masing-masing element
-titleEl.innerHTML = data.title.romaji; // contoh
+// contoh
 // element selanjutnya ......
+const getDetail = async (id) => {
+  try {
+    const res = await fetch("https://animeapi-askiahnur1.b4a.run/anime/" + id);
+    const detail = await res.json();
+    console.log(detail);
 
-const info = [
-  data.year,
-  data.format,
-  data.episodes ? +`${data.episodes} Episodes` : '',
-];
+    return detail;
+  } catch (error) {}
+};
 
-let result = '';
-info.forEach((item) => {
-  if (item) {
-    result += `<span class="bg-gray-100 text-gray-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">${item}</span>`;
-  }
-});
+const render = async () => {
+  const data = await getDetail(id);
 
-infoEl.innerHTML = result;
+  titleEl.innerHTML = data.title.romaji;
+  bannerImageEl.innerHTML = data.bannerImage;
+  console.log(bannerImageEl)
+  console.log(data.bannerImage)
+  coverImageEl.innerHTML = data.coverImage;
+  
+
+  const info = [
+    data.year, 
+    data.format, 
+    data.episodes ? +`${data.episodes} Episodes` : ""
+  ];
+
+  let result = "";
+  info.forEach((item) => {
+    if (item) {
+      result += `<span class="bg-gray-100 text-gray-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">${item}</span>`;
+    }
+  });
+
+  infoEl.innerHTML = result;
+};
+
+render();
 
 //? tambahkan script untuk menampilkan daftar genre
 // .....
